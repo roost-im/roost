08efda21545906ca686209490609e63817df82fe
David Benjamin <davidben@mit.edu>
2013-05-03 15:38:47 -0400

    Somewhat better extraction of message fields

--- subscriber.js
+++ subscriber.js
@@ -44,6 +44,13 @@ app.use(express.static(__dirname + '/static'));
 var server = http.createServer(app);
 var io = socketIo.listen(server);
 
+function zuserRealm(user) {
+  var idx = user.indexOf('@');
+  if (idx >= 0 && idx < user.length - 1)
+    return user.substring(idx + 1);
+  return zephyr.getRealm();
+}
+
 var connections = { };
 zephyr.on('notice', function(notice) {
   // Skip the random ACKs.
@@ -65,12 +72,24 @@ zephyr.on('notice', function(notice) {
     instance: notice.instance,
     sender: notice.sender,
     recipient: notice.recipient,
+    realm: zuserRealm(notice.recipient),
     auth: notice.auth,
-    // FIXME: Is this how you parse the body??
-    body: notice.body[1],
-    signature: notice.body[0],
     opcode: notice.opcode
   };
+  // TODO(davidben): Pull in the full logic from BarnOwl's
+  // owl_zephyr_get_message? It checks on default formats and the
+  // like.
+  //
+  // How much of this processing should be server-side and how much
+  // client-side? If we want to be able to search on the body, that
+  // suggests it should be in the server.
+  if (notice.body.length > 1) {
+    msg.signature = notice.body[0];
+    msg.body = notice.body[1];
+  } else {
+    msg.signature = '';
+    msg.body = notice.body[0] || '';
+  }
   for (var id in connections) {
     connections[id].emit('message', msg);
   }
